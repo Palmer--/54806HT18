bad78a64ef4b70d94a8299df8bdde91e798e60c2
Simon Palmér <simon.palmer@telia.com>
2019-01-07 23:10:17 +0100

    Work towards making the Snake game Tensor compatible.

--- snakeGame.py
+++ snakeGame.py
@@ -1,7 +1,6 @@
 #SNAKES GAME
 #SOURCE: https://gist.github.com/sanchitgangwar/2158089
-# Use ARROW KEYS to play, SPACE BAR for pausing/resuming and Esc Key for exiting
-import pygame
+import pygame as pg
 import numpy as np
 import os
 from random import randint
@@ -21,9 +20,15 @@ class SnakeGame:
     score = 0.0
     previous_input = np.array([0, 0, 0, 0])
 
+    #Draw stuff
+    draw_resolution = 20
+    screen_dim = (world_dimensions[0]*draw_resolution,
+                  world_dimensions[1]*draw_resolution)
+    snake_color = pg.Color("green")
+    food_color = pg.Color("red")
     os.environ['SDL_VIDEO_CENTERED'] = '1'
-    pygame.init()
-    screen = pygame.display.set_mode((200, 200))
+    pg.init()
+    screen = pg.display.set_mode(screen_dim)
 
     def snake_head(self):
         return self.snake[0]
@@ -37,50 +42,44 @@ class SnakeGame:
             state[food_bit] = Food_Val
         return state
 
+    def draw_state(self):
+        surface = pg.Surface(self.screen_dim)
+        for x in self.snake:
+            surface = pg.Surface(self.draw_resolution)
+            surface.fill(self.snake_color)
+            pg.draw.rect(surface, pg.Color.)
+        self.screen.
+
     def step(self, action: np.ndarray):
         if len(action) is not len(Movement_Map):
             raise ValueError("parameter action must have a length of {} but got a length of {}", len(action), len(Movement_Map))
-
-        
-        return None
-
-    while key != 27:  # While Esc key is not pressed
-        #win.border(0)
-        #win.addstr(0, 2, 'Score : ' + str(score) + ' ')  # Printing 'Score' and
-        #win.addstr(0, 27, ' SNAKE ')  # 'SNAKE' strings
-        #win.timeout(150 - (len(snake) / 5 + len(snake) / 10) % 120)  # Increases the speed of Snake as its length increases
-
-        prevKey = key  # Previous key pressed
-        key = key if event == -1 else event
-
-        if key == ord(' '):  # If SPACE BAR is pressed, wait for another
-            key = -1  # one (Pause/Resume)
-            while key != ord(' '):
-                key = win.getch()
-            key = prevKey
-            continue
-
-        #if key not in [KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN, 27]:  # If an invalid key is pressed
-        #    key = prevKey
-
         # Calculates the new coordinates of the head of the snake. NOTE: len(snake) increases.
         # This is taken care of later at [1].
-        snake.insert(0, [snake[0][0] + (key == KEY_DOWN and 1) + (key == KEY_UP and -1),
-                         snake[0][1] + (key == KEY_LEFT and -1) + (key == KEY_RIGHT and 1)])
+        action_reward = -0.01
+        done = False
+        self.snake.insert(0, (self.snake[0][0] + (action[2] and 1) + (action[0] and -1),
+                              self.snake[0][1] + (action[3] and -1) + (action[1] and 1)))
 
         # Exit if snake crosses the boundaries
-        if snake[0][0] == 0 or snake[0][0] == 19 or snake[0][1] == 0 or snake[0][1] == 59:
-            break
+        if self.snake[0][0] == 0 or self.snake[0][0] == 19 or self.snake[0][1] == 0 or self.snake[0][1] == 59:
+            action_reward = -1
+            done = True
 
         # If snake runs over itself
-        if snake[0] in snake[1:]:
-            break
+        if self.snake[0] in self.snake[1:]:
+            action_reward = -1
+            done = True
 
-        if snake[0] == food:  # When snake eats the food
-            food = []
-            score += 1
-            while food is None:
-                food = (randint(1, world_dimensions[0]-2), randint(1, world_dimensions[1]-2))  # Calculating next food's coordinates
-                if food in snake: food = None
+        # When snake eats the food
+        if self.snake[0] in self.food:
+            action_reward = 1
+            self.food = []
+            self.score += 1
+            while self.food is None:
+                self.food = (randint(1, self.world_dimensions[0]-2), randint(1, self.world_dimensions[1]-2))  # Calculating next food's coordinates
+                if food in self.snake:
+                    food = None
         else:
-            last = snake.pop()  # [1] If it does not eat the food, length decreases
+            self.snake.pop()  # [1] If it does not eat the food, length decreases
+
+        return self.get_state(), action_reward, done, ""
